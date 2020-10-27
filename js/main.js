f9c8a400188a842c81a20c4f8f6f6616168c354c
rgomez04 <rafa_3414@hotmail.com>
2020-10-27 12:51:02 -0600

    Funcionalidad mostrando los datos covid

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+//Obtener JSON COVID
+var covidJSON;
+fetch("https://api.covid19api.com/summary")
+    .then(res => res.json())
+    .then((out) => {
+        covidJSON = out.Countries;
+        covidJSON.sort(function (a, b) {
+            return b.TotalConfirmed > a.TotalConfirmed ? 1
+                : b.TotalConfirmed < a.TotalConfirmed ? -1
+                    : 0;
+        });
+
+    })
+    .catch(err => { throw err });
+
 var style = new ol.style.Style({
     fill: new ol.style.Fill({
         color: 'rgba(255, 255, 255, 0.6)',
@@ -18,13 +33,45 @@ var style = new ol.style.Style({
     }),
 });
 
+var topCasesCovidStyle = new ol.style.Style({
+    fill: new ol.style.Fill({
+        color: 'rgba(255, 0, 0, 0.6)',
+    }),
+    stroke: new ol.style.Stroke({
+        color: '#319FD3',
+        width: 1,
+    }),
+    text: new ol.style.Text({
+        font: '12px Calibri,sans-serif',
+        fill: new ol.style.Fill({
+            color: '#000',
+        }),
+        stroke: new ol.style.Stroke({
+            color: '#f00',
+            width: 3,
+        }),
+    }),
+});
+
 var vectorLayer = new ol.layer.Vector({
     source: new ol.source.Vector({
         url: 'https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson',
         format: new ol.format.GeoJSON(),
     }),
     style: function (feature) {
-        style.getText().setText(feature.get('ADMIN'));
+        var ISO_A2 = feature.get('ISO_A2');
+        fill = new ol.style.Fill();
+        var i;
+
+        for (i = 0; i < 5; i++) {
+            if (covidJSON[i].CountryCode == ISO_A2) {
+                fill.setColor(topCasesCovidStyle.getText().setText(feature.get('ADMIN')));
+                return style;
+            }
+            else {
+                fill.setColor(style.getText().setText(feature.get('ADMIN')));
+            }
+        }
         return style;
     },
 });
@@ -40,7 +87,7 @@ var map = new ol.Map({
 
 var highlightStyle = new ol.style.Style({
     stroke: new ol.style.Stroke({
-        color: '#f00',
+        color: '#0f0',
         width: 1,
     }),
     fill: new ol.style.Fill({
@@ -52,7 +99,7 @@ var highlightStyle = new ol.style.Style({
             color: '#000',
         }),
         stroke: new ol.style.Stroke({
-            color: '#f00',
+            color: '#0f0',
             width: 3,
         }),
     }),
@@ -68,9 +115,6 @@ var featureOverlay = new ol.layer.Vector({
 });
 
 var highlight;
-//Obtener JSON COVID
-
-//Señalar países con el mayor número de casos confirmados totales
 
 var displayFeatureInfo = function (pixel) {
     var feature = map.forEachFeatureAtPixel(pixel, function (feature) {
@@ -78,10 +122,18 @@ var displayFeatureInfo = function (pixel) {
     });
 
     var info = document.getElementById('info');
-    //Juntar datos JSON
-
     if (feature) {
-        info.innerHTML = feature.get('ISO_A3') + ': ' + feature.get('ADMIN');
+        var tConf, tDeat, tRec;
+        //Conseguir los datos por covid
+        covidJSON.forEach(function (data, index) {
+            if (feature.get('ISO_A2') === data.CountryCode) {
+                tConf = data.TotalConfirmed;
+                tDeat = data.TotalDeaths;
+                tRec = data.TotalRecovered;
+            }
+        });
+        info.innerHTML = feature.get('ISO_A3') + ': ' + feature.get('ADMIN')
+            + ', Total Confirmed: ' + tConf + ', Total Deaths: ' + tDeat + ', Total Recovered: ' + tRec;
     } else {
         info.innerHTML = '&nbsp;';
     }
